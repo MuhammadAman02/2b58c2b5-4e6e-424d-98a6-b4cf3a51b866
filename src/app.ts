715a33f04f211afdc0affe3edbcfc4dd8033f391
MuhammadAman02 <mohammada@hst.ie>
2025-07-08 19:15:35 +0500

    update file

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,17 @@
-import Fastify from "fastify";
-import root from "./routes/root";
+import Fastify from 'fastify';
+import { fruitRoutes } from './routes/fruit.route';
 
 const app = Fastify({
-  logger: true,
+  logger: true
 });
 
 // Register routes
-app.register(root, { prefix: "/" });
+app.register(fruitRoutes, { prefix: '/api' });
 
 // Global error handler
 app.setErrorHandler((error, request, reply) => {
   app.log.error(error);
-  reply.status(500).send({ error: "Internal Server Error" });
+  reply.status(500).send({ error: 'Internal Server Error' });
 });
 
 export default app;
