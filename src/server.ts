1f09590e7cefef17212ee18ccc754383330eb26e
MuhammadAman02 <mohammada@hst.ie>
2025-07-08 19:15:37 +0500

    update file

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,9 @@
-import app from "./app"; // ✅ This is a Fastify instance
+import app from './app';
 
 const start = async () => {
   try {
-    await app.listen({ port: 3000, host: "0.0.0.0" });
-    console.log("🚀 Server listening at http://localhost:3000");
+    await app.listen({ port: 3000, host: '0.0.0.0' });
+    console.log('Server running on http://localhost:3000');
   } catch (err) {
     app.log.error(err);
     process.exit(1);
